9277af2c07abc91ac004bae6dc4d1a36555ae580
andrezszambrano <andreskilla2000@gmail.com>
2022-04-13 15:18:43 -0300

    Constructor is added to App component, along with the loggedIn boolean state.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react'
 import GetUsers from "./components/Getusers";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {loggedIn: false};
+    }
+
     render() {
         return (
             <div>
