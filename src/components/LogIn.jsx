0b85721b83008a47a4d12470a527fe08a8beab69
andrezszambrano <andreskilla2000@gmail.com>
2022-04-13 17:33:27 -0300

    LogIn Text fields now save the inputs they receive.

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -5,6 +5,13 @@ class LogIn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {userName: null, password: null};
+        this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+    }
+
+    handleTextFieldChange(e) {
+        this.setState({
+            textFieldValue: e.target.value
+        });
     }
 
     render() {
@@ -12,10 +19,10 @@ class LogIn extends React.Component {
             <div>
                 <h1>Spotifiubyfy</h1>
                 <h5>Hello there admin! Please, log in.</h5>
-                <TextField label="Username"/>
-                <TextField label="Password"/>
+                <TextField label="Username" onChange={this.handleTextFieldChange}/>
+                <TextField label="Password" onChange={this.handleTextFieldChange}/>
             </div>
-    );
+        );
     }
 }
 
