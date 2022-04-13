fbe69c0f0ae312988218e25a5a9caae78871bfa6
andrezszambrano <andreskilla2000@gmail.com>
2022-04-13 17:42:04 -0300

    A button is added. When it is clicked, it prints to console.

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 class LogIn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {userName: null, password: null};
         this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+        this.handleRequest = this.handleRequest.bind(this);
     }
 
     handleTextFieldChange(e) {
         this.setState({
             textFieldValue: e.target.value
         });
+    }
+
+    handleRequest(e) {
+        console.log('Clicked!\n');
     }
 
     render() {
@@ -21,6 +27,7 @@ class LogIn extends React.Component {
                 <h5>Hello there admin! Please, log in.</h5>
                 <TextField label="Username" onChange={this.handleTextFieldChange}/>
                 <TextField label="Password" onChange={this.handleTextFieldChange}/>
+                <div><Button variant="contained" onClick={this.handleRequest}>Contained</Button></div>
             </div>
         );
     }
