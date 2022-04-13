a79c3758a9f5e8e7e152cef0eb345e62269a9429
andrezszambrano <andreskilla2000@gmail.com>
2022-04-13 15:30:27 -0300

    LogIn component is created. It has two states: the username and password. render() returns a hello world.

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class LogIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {userName: null, password: null};
+    }
+
+    render() {
+        return (
+            <div>
+            Hello World!
+            </div>
+        );
+    }
+}
