1964850d2bfa3a6959eaab902896185838cde5e3
RVLareu <rlareu@fi.uba.ar>
2022-04-21 18:24:41 -0300

    LogIn now a function with hooks to update pass and username, not fully functional yet

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,36 +1,26 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
-class LogIn extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {userName: null, password: null};
-        this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
-        this.handlePressedButton = this.handlePressedButton.bind(this);
-    }
+export default function LogIn() {
+    const [username, setUsername] = useState([])
+    const [password, setpassword] = useState([])
 
-    handleTextFieldChange(changedField, e) {
-        this.setState({
-            [changedField]: e.target.value
-        });
-    }
-
-    handlePressedButton(e) {
-        this.props.requestLogIn(this.state.userName, this.state.password);
-    }
-
-    render() {
+            
         return (
-            <div>
+            <div> 
                 <h5>Hello there admin! Please, log in.</h5>
-                <TextField label="Username" onChange={(e) => this.handleTextFieldChange("userName", e)}/>
-                <TextField label="Password" type="password" onChange={(e) => this.handleTextFieldChange("password", e)}/>
-                <div><Button variant="contained" onClick={this.handlePressedButton}>Log in</Button></div>
-                <div><h3 style={{color: 'red'}}>{this.props.error}</h3></div>
+                <TextField label="Username" onChange={(e) => setUsername(e.target.value)}/>
+                <TextField label="Password" type="password" onChange={(e) => setpassword(e.target.value)}/>
+                <div>
+                    <Button variant="contained" onClick={()=>{console.log("REQUESTED LOGIN", password, username)}}>Log in</Button>
+                </div>
+                <div>
+                    <h3 style={{color: 'red'}}>{"ERROR"}</h3>
+                </div>
             </div>
         );
-    }
+    
 }
 
-export default LogIn;
