cebe9c1bd9931ba5de94f7545523bc04d01856ff
andrezszambrano <andreskilla2000@gmail.com>
2022-04-22 10:58:34 -0300

    Other LogIn tests are added. Some of them don't pass yet, refactor is needed.

--- src/tests/LogIn.test.js
+++ src/tests/LogIn.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import {fireEvent, render} from '@testing-library/react'
 import LogIn from '../components/LogIn'
 
 test('LogIn components are rendered correctly', () => {
@@ -9,3 +9,25 @@ test('LogIn components are rendered correctly', () => {
     component.getByLabelText('Password');
     component.getByText('Log in');
 })
+
+test('LogIn button handler is called once when the button is pressed', () => {
+    const mockHandler = jest.fn();
+    const component = render(<LogIn requestLogIn={mockHandler}/>);
+    const button = component.getByText('Log in');
+    fireEvent.click(button);
+
+    expect(mockHandler).toHaveBeenCalledTimes(1);
+})
+
+test('LogIn shows error message when trying to log in without inserting text to any text fields', () => {
+    const mockHandler = jest.fn();
+    const component = render(<LogIn requestLogIn={mockHandler}/>);
+    const button = component.getByText('Log in');
+    fireEvent.click(button);
+
+    component.getByText('Fill both username and password fields');
+}) //should pass
+
+test('LogIn fetches user data accordingly', () => {
+
+}) //need refactor first
