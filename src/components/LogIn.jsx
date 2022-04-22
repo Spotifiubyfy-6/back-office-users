9b44ee76b34ac61969abaacf3728a4e54ebc5a39
andrezszambrano <andreskilla2000@gmail.com>
2022-04-22 11:56:27 -0300

    Fixed: when a user does not put text in username or password, no request is done and an error message is shown

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -10,20 +10,22 @@ export default function LogIn(props) {
     const [loginError, setLoginError] = useState([])
 
     function requestLogin(username, password) {
-        if(username == null || password == null) {
+        if(Object.keys(username).length == 0 || Object.keys(password).length == 0) {
+            //si entra acá
             setLoginError("Fill both username and password fields");
-            return;
-        }
-        getToken(username, password)
-        .then((res) => {
+            console.log('here');
+        } else {
+            getToken(username, password)
+           .then((res) => {
             var auth = res.data.token_type + ' ' + res.data.access_token;
             props.setAuthorization(auth);
             props.setLoginState(true)
-        }).catch((error) => {
+           }).catch((error) => {
             let stringError = error.toString();
             console.log(stringError);
             setLoginError(stringError)
-        })
+           })
+        }
     }
         
     return (
