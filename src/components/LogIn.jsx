654b926bccba99e7ec8ea3fcd22c840abbf6e875
RVLareu <rlareu@fi.uba.ar>
2022-04-22 10:38:37 -0300

    Login fetches the token and handles it to App

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -2,25 +2,43 @@ import React from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
+import { getToken } from '../functions/getTokenRequest';
 
-export default function LogIn() {
-    const [username, setUsername] = useState([])
-    const [password, setpassword] = useState([])
+export default function LogIn(props) {
+    const [username, setUsername] = useState([]);
+    const [password, setpassword] = useState([]);
+    const [loginError, setLoginError] = useState([])
 
-            
-        return (
-            <div> 
-                <h5>Hello there admin! Please, log in.</h5>
-                <TextField label="Username" onChange={(e) => setUsername(e.target.value)}/>
-                <TextField label="Password" type="password" onChange={(e) => setpassword(e.target.value)}/>
-                <div>
-                    <Button variant="contained" onClick={()=>{console.log("REQUESTED LOGIN", password, username)}}>Log in</Button>
-                </div>
-                <div>
-                    <h3 style={{color: 'red'}}>{"ERROR"}</h3>
-                </div>
+    function requestLogin(username, password) {
+        if(username == null || password == null) {
+            setLoginError("Fill both username and password fields");
+            return;
+        }
+        getToken(username, password)
+        .then((res) => {
+            var auth = res.data.token_type + ' ' + res.data.access_token;
+            props.setAuthorization(auth);
+            props.setLoginState(true)
+        }).catch((error) => {
+            let stringError = error.toString();
+            console.log(stringError);
+            setLoginError(stringError)
+        })
+    }
+        
+    return (
+        <div> 
+            <h5>Hello there admin! Please, log in.</h5>
+            <TextField label="Username" onChange={(e) => setUsername(e.target.value)}/>
+            <TextField label="Password" type="password" onChange={(e) => setpassword(e.target.value)}/>
+            <div>
+                <Button variant="contained" onClick={()=>{requestLogin(username, password)}}>Log in</Button>
             </div>
-        );
+            <div>
+                <h3 style={{color: 'red'}}>{loginError}</h3>
+            </div>
+        </div>
+    );
     
 }
 
