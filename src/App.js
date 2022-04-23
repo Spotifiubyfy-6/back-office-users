662344e9c56aad7b6368ab88ec8d513b005430c1
andrezszambrano <andreskilla2000@gmail.com>
2022-04-23 11:47:52 -0300

    Root creates an apiHandler that is passed to the App component as a prop. This is the same apiHandler that the app gives to its children

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
 import UsersTable from "./components/UsersTable";
 import LogIn from "./components/LogIn"
+import APIHandler from "./classes/APIHandler"
 import { useState } from "react";
 
-function App() {
+function App(props) {
 
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [authorization, setAuthorization] = useState([])
-
+    const [loggedIn, setLoggedIn] = useState(false);
     // SEE REACT ROUTER
     if (loggedIn) {
-        return <UsersTable authorization = {authorization}/>
+        return <UsersTable apiHandler = {props.apiHandler}/>
     } else {
         return (
             <div>
                 <h1>Spotifiubyfy</h1>
-                <LogIn setLoginState = {setLoggedIn} setAuthorization = {setAuthorization} />
+                <LogIn apiHandler = {props.apiHandler} setLoginState = {setLoggedIn}/>
             </div>
         ); 
     }  
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import APIHandler from './classes/APIHandler'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+let apiHandler = new APIHandler();
+root.render(<App apiHandler = {apiHandler}/>);
