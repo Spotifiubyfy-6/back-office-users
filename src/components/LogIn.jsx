96c30c3753720e4bb84a8761fa3d8e8d17069c53
andrezszambrano <andreskilla2000@gmail.com>
2022-04-23 11:08:06 -0300

    When LogIn button is pressed, the handler asks the api to connect with the given username and password, and if the connection is succesful the Apps state is changed, otherwise the connection error is shown on the screen.

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -2,35 +2,32 @@ import React from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
-import { getToken } from '../functions/getTokenRequest';
+import APIHandler from '../classes/APIHandler.js'
 
 export default function LogIn(props) {
     const [username, setUsername] = useState('');
-    const [password, setpassword] = useState('');
+    const [password, setPassword] = useState('');
     const [loginError, setLoginError] = useState('')
 
     function requestLogin(username, password) {
-        if(username === '' || password === '') {
+        if (username === '' || password === '') {
             setLoginError("Fill both username and password fields");
             return;
         }
-        getToken(username, password)
+        props.apiHandler.logIn(username, password)
         .then((res) => {
-        var auth = res.data.token_type + ' ' + res.data.access_token;
-        props.setAuthorization(auth);
-        props.setLoginState(true)
+            props.setLoginState(true);
         }).catch((error) => {
             let stringError = error.toString();
-            console.log(stringError);
-            setLoginError(stringError)
+            setLoginError(stringError);
         })
     }
-        
+
     return (
         <div> 
             <h5>Hello there admin! Please, log in.</h5>
             <TextField label="Username" onChange={(e) => setUsername(e.target.value)}/>
-            <TextField label="Password" type="password" onChange={(e) => setpassword(e.target.value)}/>
+            <TextField label="Password" type="password" onChange={(e) => setPassword(e.target.value)}/>
             <div>
                 <Button variant="contained" onClick={()=>{requestLogin(username, password)}}>Log in</Button>
             </div>
