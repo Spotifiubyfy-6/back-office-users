7d09f3f5cf9b8cdbcef59f109ce89141fd69e771
RVLareu <rlareu@fi.uba.ar>
2022-04-26 15:38:32 -0300

    added Route in index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import APIHandler from './classes/APIHandler'
 import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let apiHandler = new APIHandler();
 root.render(
     <BrowserRouter>
-        <App apiHandler = {apiHandler}/>
+        <Routes>
+            <Route path="/app" element={<App apiHandler = {apiHandler}/>} />    
+        </Routes>
     </BrowserRouter>
     );
