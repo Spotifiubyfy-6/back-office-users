d5fe75ee0213b48f7d704fba0a06f7f7c6c53a1a
andrezszambrano <andreskilla2000@gmail.com>
2022-04-28 13:02:57 -0300

    If ErrorBoxComponent receives an empty string, it returns null

--- src/components/ErrorBox.jsx
+++ src/components/ErrorBox.jsx
@@ -3,6 +3,9 @@ import {Alert} from "@mui/material";
 
 
 export default function ErrorBox(props) {
+    if (props.errorString == '') {
+        return null;
+    }
     return (<div>
             <Alert variant="filled" severity="error">
                 Error: {props.errorString}
