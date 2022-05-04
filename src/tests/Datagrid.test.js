be6883dcaaf8745e3dbb5350e37c0979530a2e7c
andrezszambrano <andreskilla2000@gmail.com>
2022-05-03 22:10:56 -0300

    Admins not having a delete button is tested.

--- src/tests/Datagrid.test.js
+++ src/tests/Datagrid.test.js
@@ -29,6 +29,21 @@ test('APIHandler method delete is called once after delete button is clicked', a
     await waitFor(() => expect(apiMock.deleteUser).toHaveBeenCalledTimes(1));
 })
 
+test('Admins do not have delete button', async () => {
+    const apiMock = {
+        deleteUser: jest.fn(() => Promise.resolve(returnData.SUCCESS))
+    };
+    const rows = [
+        { id: 1, user_type: 'admin', username: 'admin', email: 'admin@gmail.com'},
+        { id: 2, user_type: 'admin', username: 'pedro', email: 'pedro@gmail.com'},
+        { id: 3, user_type: 'listener', username: 'paco', email: 'paco@gmail.com'},
+    ];
+    render(<DataGridUsers apiHandler={apiMock} rows={rows}/>);
+
+    expect(() => screen.getByRole('button', {name: 'deleteUser1'})).toThrow();
+    expect(() => screen.getByRole('button', {name: 'deleteUser2'})).toThrow();
+})
+
 test('If server is down when calling api delete method, error message is shown on screen',
     async () => {
         const apiMock = {
