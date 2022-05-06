25a0ae8c9011ab8855f5f21c0dfc190bf7b30121
andrezszambrano <andreskilla2000@gmail.com>
2022-05-05 23:34:40 -0300

    ViewUserButton is refactored so it can be reused as ImageButton. It receives obligatory props onclick, src image, alt text, width and height in order to render.

---
+++ src/components/Datagrid/ImageButton.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function ImageButton(props) {
+    if (!props.onClick || !props.alt || !props.src || !props.height || !props.width)
+        return null;
+    return <div>
+        <button type = "button" onClick={() => props.onClick(props.params)}>
+            <input type="image" alt={props.alt} height={props.height} width={props.width}
+            src={props.src}/>
+        </button>
+    </div>
+}
