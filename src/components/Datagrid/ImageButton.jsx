bcc5020a4b3efbb67a36a5a10bc76472b768906b
andrezszambrano <andreskilla2000@gmail.com>
2022-05-05 23:50:12 -0300

    ImageButton component sets a label in the button for testing purposes

--- src/components/Datagrid/ImageButton.jsx
+++ src/components/Datagrid/ImageButton.jsx
@@ -4,7 +4,7 @@ export default function ImageButton(props) {
     if (!props.onClick || !props.alt || !props.src || !props.height || !props.width)
         return null;
     return <div>
-        <button type = "button" onClick={() => props.onClick(props.params)}>
+        <button type = "button" onClick={() => props.onClick(props.params)} aria-label={props.arialLabel}>
             <input type="image" alt={props.alt} height={props.height} width={props.width}
             src={props.src}/>
         </button>
