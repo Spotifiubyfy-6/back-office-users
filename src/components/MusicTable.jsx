2fb7d4e8b4852759f7e8ec2b968dc620f4b45c08
RVLareu <rlareu@fi.uba.ar>
2022-05-07 21:17:56 -0300

    form removed from musica table

--- src/components/MusicTable.jsx
+++ src/components/MusicTable.jsx
@@ -1,61 +1,23 @@
 import React from 'react'
 import { DataGrid } from '@mui/x-data-grid';
-import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, TextField, Box } from '@mui/material';
-
+import {TextField, Box } from '@mui/material';
 import { useState } from 'react';
 
 
 
-const rowsData = [
-    {
-        id: 1,
-        name: 'All You Need is Love',
-        duration: 3,
-        artist: 'The Beatles',
-        album: "Yellow Submarine",
-        genre: "Rock"
-    },
-    {
-        id: 2,
-        name: 'All Together Now',
-        duration: 4,
-        artist: 'The Beatles',
-        album: "Yellow Submarine",
-        genre: "Rock"
-    },
-    {
-        id: 3,
-        name: 'Fixing A Hole',
-        duration: 5,
-        artist: 'The Beatles',
-        album: "Sgt. Pepper",
-        genre: "Rock"
-    },
-    {
-        id: 4,
-        name: 'Coming In From The Cold',
-        duration: 6,
-        artist: 'Bob Marley',
-        album: "Uprising",
-        genre: "Reagge"
-    }
-]
 
 
 
 
 
-const columns =[
-        { field: 'name', width: 150 },
-        { field: 'artist', width: 150 },
-        { field: 'duration', width: 150 },
-        { field: 'album', width: 150 },
-        { field: 'genre', width: 150 }
-    ]
 
-export default function MusicTable() {
 
-    const [searchOption, setSearchOption] = useState("name");
+
+export default function MusicTable(props) {
+
+    const rowsData = props.rows
+    
+
     const [rows, setRows] = useState(rowsData)
 
 
@@ -65,39 +27,18 @@ export default function MusicTable() {
         if (event.target.value === '') {
             setRows(rowsData)
         } else {
-            setRows(rowsData.filter((song) => song[searchOption].includes(event.target.value)))
+            setRows(rowsData.filter((song) => song[props.searchParameter].includes(event.target.value)))
         }
     }
-    const handleChange = (event) => {
-        console.log(event.target.value)
-        setSearchOption(event.target.value);
-    };
     return (
         <div >
         
             <Box display="flex" sx={{height:"500px"}} >
-                <Box  padding="0 20px 0 0">
-                    <FormControl >
-                        <FormLabel id="demo-radio-buttons-group-label">Search options</FormLabel>
-                        <RadioGroup
-                            aria-labelledby="demo-radio-buttons-group-label"
-                            defaultValue="female"
-                            name="radio-buttons-group"
-                            value={searchOption}
-                            onChange={handleChange}
-                        >
-                            <FormControlLabel value="name" control={<Radio />} label="Name" />
-                            <FormControlLabel value="artist" control={<Radio />} label="Artist" />
-                            <FormControlLabel value="album" control={<Radio />} label="Album" />
-                            <FormControlLabel value="genre" control={<Radio />} label="Genre" />
-                        </RadioGroup>
-                    </FormControl>
-                </Box>
                 <Box display="flex" flexDirection="column" sx={{width:"100%"}}>
                     <TextField fullWidth label="Search" id="search" onChange={handleSearchBarChange} />
                     <DataGrid         
                     rows={rows}
-                    columns={columns}
+                    columns={props.columns}
                     disableSelectionOnClick
                     style={{ height: "500px", widht: "100%"}}/>
                 </Box>
