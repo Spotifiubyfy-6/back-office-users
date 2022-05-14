b4d0eaea8d3cdbd128ba02e0737b6e5dcc60e721
andrezszambrano <andreskilla2000@gmail.com>
2022-05-14 13:03:57 -0300

    A line chart is shown on metrics screen

--- src/routes/metrics.jsx
+++ src/routes/metrics.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import "../components/Metrics/style.css"
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const options = {
+  responsive: true,
+ maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'top'
+    },
+  },
+  scales: {
+    y: {
+      title: {
+        display: true,
+        text: 'Quantity of LogIns'
+      }
+    },
+    x: {
+      title: {
+        display: true,
+        text: 'Days ago'
+      }
+    }
+  }
+};
+
+const labels = ['6', '5', '4', '3', '2', '1', '0'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'LogIns with user and password',
+      data: labels.map(() => Math.floor(Math.random() * 1000)),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)'
+    },
+  ],
+};
 
 export default function metrics() {
   return (
-    <div>SOYmetrics</div>
-  )
+  <div className="container">
+    <div className="row">
+      <div> <Line width={750} height={200} options={options} data={data} /> </div>
+    </div>
+  </div>);
 }
