8a76aeac2df9b8cac18b20d38cef8c3fe05e3e9d
RVLareu <rlareu@fi.uba.ar>
2022-05-15 14:12:50 -0300

    artist has a button to see albums and songs from artist. Uses query string

--- src/routes/artists.jsx
+++ src/routes/artists.jsx
@@ -1,14 +1,56 @@
 import React from 'react'
 import MusicTable from '../components/MusicTable'
-import { Container } from '@mui/material'
-
+import { Container, Button } from '@mui/material'
+const queryString = require('query-string')
 
 const columns =[
     { field: 'name', width: 150 },
     
     { field: 'albums', width: 150 },
     { field: 'songs', width: 150 },
-    { field: 'genre', width: 150 }
+    { field: 'genre', width: 150 },
+    {
+      field: 'viewSongs',
+      headerName: 'View Songs',
+      width: 150,
+      renderCell: (params) => {
+         return <strong>
+          <Button variant="contained" color="secondary" size="small" style={{marginLeft: 16}}
+                  aria-label={'View Songs'}
+                  onClick={() => {
+                      const parsed = queryString.parse(window.location.search);
+                      parsed.artist_name = params.row.name
+                      window.location.search = queryString.stringify(parsed)
+                      window.location = '/songs/?' +  queryString.stringify(parsed)
+                      
+
+                  }}>
+          View Songs
+          </Button>
+        </strong>
+      },
+    },
+    {
+      field: 'viewAlbums',
+      headerName: 'View Albums',
+      width: 150,
+      renderCell: (params) => {
+         return <strong>
+          <Button variant="contained" color="secondary" size="small" style={{marginLeft: 16}}
+                  aria-label={'View Albums'}
+                  onClick={() => {
+                      const parsed = queryString.parse(window.location.search);
+                      parsed.artist_name = params.row.name
+                      window.location.search = queryString.stringify(parsed)
+                      window.location = '/albums/?' +  queryString.stringify(parsed)
+                      
+
+                  }}>
+          View Albums
+          </Button>
+        </strong>
+      },
+    }
 ]
 
 
@@ -47,7 +89,7 @@ const rows = [
 export default function artists(props) {
   return (
     <Container maxWidth={false}>
-      <MusicTable  apiHandler = {props.apiHandler} searchParameter = {"artist"} columns = {columns} rows = {rows} />
+      <MusicTable  apiHandler = {props.apiHandler} searchParameter = {"name"} columns = {columns} rows = {rows} />
     </Container>
   )
 }
