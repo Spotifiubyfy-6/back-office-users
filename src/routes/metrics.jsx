30d00068f1626cd99af0977ca3f5675ca958471f
andrezszambrano <andreskilla2000@gmail.com>
2022-05-20 00:26:40 -0300

    Users created chart is added

--- src/routes/metrics.jsx
+++ src/routes/metrics.jsx
@@ -6,9 +6,12 @@ export default function metrics(props) {
   return (
   <div className="container">
     <div className="row">
+      <div> <LineChartWithMonthSelector xTitle={'Days ago'} yTitle={'Quantity of new users'}
+                                        title={'Users created'} color={'rgb(20, 10, 255)'}
+                                        width={750} height={200} apiHandler={props.apiHandler} metrics_id={1}/> </div>
       <div> <LineChartWithMonthSelector xTitle={'Days ago'} yTitle={'Quantity of LogIns'}
                                         title={'LogIns with user and password'} color={'rgb(255, 99, 132)'}
-          width={750} height={200} apiHandler={props.apiHandler} metrics_id={1}/> </div>
+          width={750} height={200} apiHandler={props.apiHandler} metrics_id={2}/> </div>
     </div>
   </div>);
 }
