f15819dfb5669bd93ca0abd56e7aaf1e322b4c9a
RVLareu <rlareu@fi.uba.ar>
2022-05-24 14:36:22 -0300

    rename songs columns and search parameter

--- src/routes/songs.jsx
+++ src/routes/songs.jsx
@@ -3,52 +3,19 @@ import MusicTable from '../components/MusicTable'
 import { Container } from '@mui/material'
 
 const columns =[
-  { field: 'name', width: 150 },
-  { field: 'artist', width: 150 },
-  { field: 'duration', width: 150 },
-  { field: 'album', width: 150 },
-  { field: 'genre', width: 150 }
+  { field: 'id', width: 150 },
+  { field: 'song_name', width: 150 },
+  { field: 'song_description', width: 150 },
+  { field: 'storage_name', width: 150 },
+  { field: 'album_id', width: 150 }
 ]
 
-const rows = [
-{
-    id: 1,
-    name: 'All You Need is Love',
-    duration: 3,
-    artist: 'The Beatles',
-    album: "Yellow Submarine",
-    genre: "Rock"
-},
-{
-    id: 2,
-    name: 'All Together Now',
-    duration: 4,
-    artist: 'The Beatles',
-    album: "Yellow Submarine",
-    genre: "Rock"
-},
-{
-    id: 3,
-    name: 'Fixing A Hole',
-    duration: 5,
-    artist: 'The Beatles',
-    album: "Sgt. Pepper",
-    genre: "Rock"
-},
-{
-    id: 4,
-    name: 'Coming In From The Cold',
-    duration: 6,
-    artist: 'Bob Marley',
-    album: "Uprising",
-    genre: "Reagge"
-}
-]
+
 export default function songs(props) {
 
   return (
     <Container maxWidth={false}>
-      <MusicTable  apiHandler = {props.apiHandler} searchParameter = {"name"} columns = {columns} rows = {rows}/>
+      <MusicTable  apiHandler = {props.apiHandler} searchParameter = {"songs"} columns = {columns} />
     </Container>
   )
 }
